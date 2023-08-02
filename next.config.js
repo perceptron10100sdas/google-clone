452723286c13d8d50c4483427575b7b012ba0c31
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-08-02 18:31:02 +0530

    production changes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     experimental: {
       appDir: true,
     },
+    eslint: {
+      ignoreDuringBuilds: true,
+  },
     images: {
       domains: ["upload.wikimedia.org"],
     },
