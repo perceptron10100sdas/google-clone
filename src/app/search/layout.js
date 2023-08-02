938c95ff9b6335570f396ff3bc0f8b72e8a6e160
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-08-02 08:01:32 +0530

    search for web and image done

---
+++ src/app/search/layout.js
@@ -0,0 +1,13 @@
+
+import Searchheader from "@sambhav/components/Searchheader";
+import "./../globals.css";
+
+export default function SearchLayout({ children }) {
+  return (
+    <div>
+      <Searchheader />
+      {children}
+      {/* Footer */}
+    </div>
+  );
+}
