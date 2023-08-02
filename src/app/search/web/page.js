e45b89ef48c44fa20ed41f6e63fdb51f171e8366
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-08-02 08:01:48 +0530

    2

---
+++ src/app/search/web/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function websearch() {
+  return (
+    <div>
+<p>websearch</p>
+    </div>
+  )
+}
---
+++ src/components/Searchheader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Searchhead() {
+  return (
+    <div>
+    <h1>HomeHeader</h1>
+    </div>
+  )
+}
